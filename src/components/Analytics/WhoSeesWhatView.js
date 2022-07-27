49719d91ac8c636e1007ac15ed0b1efec0664d05
romaissalmh <hr_kessi@esi.dz>
2022-07-27 14:04:05 +0200

    added advertiser in who sees what

--- src/components/Analytics/WhoSeesWhatView.js
+++ src/components/Analytics/WhoSeesWhatView.js
@@ -6,17 +6,17 @@ import {api} from '../../scripts/network'
 import { useIntl } from 'react-intl';
 import AdCard from '../Charts/AdCard';
 import PieChart from '../Charts/PieChart';
-
+import {TopAdvertisers} from "../Charts/variables/data"
 const WhoSeesWhat = () => {
     const intl = useIntl();
     const [modalDemo1, setModalDemo1] = useState(false);
     const [modalDemo2, setModalDemo2] = useState(false);
-
     let colors = ["rgb(176,43,48)", "rgb(223,227,10)", "rgb(155,177,211)" ,"rgb(55,52,59)","rgb(177,100,146)", "rgb(56,56,116)", "rgb(22,179,183)","rgb(132,54,146)", "rgb(70,160,245)", "rgb(227,161,135)","rgb(65,163,58)","rgb(21,45,72)","rgb(148,67,30)","rgb(252,105,9)","rgb(219,223,241)" ]    
     const [adsPart1,setAdsPart1] = useState({
         data : [],
         age:'18-24',
         gender:'male',
+        advertiser:"all",
         candidate:'',
         loading:true, 
 
@@ -27,7 +27,10 @@ const WhoSeesWhat = () => {
         age:'35-44',
         gender:'female',
         candidate:'',
+        advertiser:0,
         loading:true,
+        advertiser:"all",
+
 
     })
 
@@ -37,7 +40,7 @@ const WhoSeesWhat = () => {
         gender:'male',
         candidate:'',
         loading:true,
-
+        advertiser:"all",
     })
   
     const [adsPerSocialIssuesPart2,setAdsPerSocialIssuesPart2] = useState({
@@ -46,21 +49,21 @@ const WhoSeesWhat = () => {
         gender:'male',
         candidate:'',
         loading:true,
-
+        advertiser:"all",
     })
   
   
     useEffect(() => {
-        loadAdsPerSocialIssuesPart1('18-24','male')
-        loadAdsPerSocialIssuesPart2('35-44','female')
+        loadAdsPerSocialIssuesPart1('18-24','male','all')
+        loadAdsPerSocialIssuesPart2('35-44','female','all')
 
     }, [])
   
     const toggleModal1 = (social_issue) => {
         adsPart1.age = adsPerSocialIssuesPart1.age
         adsPart1.gender = adsPerSocialIssuesPart1.gender
+        adsPart1.advertiser = adsPerSocialIssuesPart1.advertiser
         adsPart1.social_issue = social_issue !== "Aucune catégorie" ? social_issue : "[]"
-        console.log(adsPart1.social_issue)
         setModalDemo1(!modalDemo1);
         loadAdsPart1(adsPart1.age , adsPart1.gender, adsPart1.social_issue)
     }
@@ -68,21 +71,23 @@ const WhoSeesWhat = () => {
     const toggleModal2 = (social_issue) => {
         adsPart2.age = adsPerSocialIssuesPart2.age
         adsPart2.gender = adsPerSocialIssuesPart2.gender
+        adsPart2.advertiser = adsPerSocialIssuesPart2.advertiser
         adsPart2.social_issue =social_issue !== "Aucune catégorie" ? social_issue : "[]"
         setModalDemo2(!modalDemo2);
-        loadAdsPart2(adsPart2.age , adsPart2.gender, adsPart2.social_issue)
+        loadAdsPart2(adsPart2.age , adsPart2.gender, adsPart2.social_issue,adsPart2.advertiser)
     }
 
 
-     const loadAdsPart1 = useCallback(async (age,gender,social_issue) => {
+     const loadAdsPart1 = useCallback(async (age,gender,social_issue,advertiser) => {
         setAdsPart1({
             data:[],
             loading:true,
             age:age,
             gender:gender,
-            social_issue:social_issue
+            social_issue:social_issue,
+            advertiser:advertiser
         })
-        const data = await fetchAdsPart1(age,gender,social_issue)
+        const data = await fetchAdsPart1(age,gender,social_issue,advertiser)
       
 
         setAdsPart1({
@@ -90,18 +95,20 @@ const WhoSeesWhat = () => {
             loading:false,
             age:age,
             gender:gender,
-            social_issue:social_issue
+            social_issue:social_issue,
+            advertiser:advertiser
         })
 
     })
 
-    const loadAdsPart2 = useCallback(async (age, gender, social_issue) => {
+    const loadAdsPart2 = useCallback(async (age, gender, social_issue,advertiser) => {
         setAdsPart2({
             data:[],
             loading:true,
             age:age,
             gender:gender,
-            social_issue:social_issue
+            social_issue:social_issue,
+            advertiser:advertiser
 
         })
         const data = await fetchAdsPart2(age, gender, social_issue)
@@ -112,22 +119,24 @@ const WhoSeesWhat = () => {
             loading:false,
             age:age,
             gender:gender,
-            social_issue:social_issue
+            social_issue:social_issue,
+            advertiser:advertiser
         })
 
     })
 
-    const loadAdsPerSocialIssuesPart1 = useCallback(async (age,gender) => {
+    const loadAdsPerSocialIssuesPart1 = useCallback(async (age,gender,advertiser) => {
         setAdsPerSocialIssuesPart1({
             data:[],
             loading:true,
             age:age,
             gender:gender,
-
+            advertiser:advertiser
         })
+    
         let labels = []
         let reach = []
-        const data = await fetchAdsPerSocialIssuesPart1(age,gender)
+        const data = await fetchAdsPerSocialIssuesPart1(age,gender,advertiser)
         for(let d in data){
             labels.push(d)
             reach.push(data[d])
@@ -139,20 +148,21 @@ const WhoSeesWhat = () => {
             loading:false,
             age:age,
             gender:gender,
+            advertiser:advertiser,
             labels : labels
 
         })
 
     })
-    const loadAdsPerSocialIssuesPart2 = useCallback(async (age,gender) => {
+    const loadAdsPerSocialIssuesPart2 = useCallback(async (age,gender,advertiser) => {
         setAdsPerSocialIssuesPart2({
             data:[],
             loading:true,
             age:age,
             gender:gender,
-
+            advertiser:advertiser,
         })
-        const data = await fetchAdsPerSocialIssuesPart2(age,gender)
+        const data = await fetchAdsPerSocialIssuesPart2(age,gender,advertiser)
         let labels = []
         let reach = []
         for(let d in data){
@@ -167,6 +177,7 @@ const WhoSeesWhat = () => {
             loading:false,
             age:age,
             gender:gender,
+            advertiser:advertiser,
             labels:labels
         })
 
@@ -174,9 +185,9 @@ const WhoSeesWhat = () => {
 
     // fetching data from the server
 
-    const fetchAdsPart1 =  async (age, gender, social_issue) => {
+    const fetchAdsPart1 =  async (age, gender, social_issue,advertiser) => {
         let stats 
-        await api.get(`api/demographicDistribution/entitiesTargetingAgeGender/`+age+`/`+gender+`/`+social_issue)
+        await api.get(`api/demographicDistribution/entitiesTargetingAgeGender/`+age+`/`+gender+`/`+social_issue+`/`+advertiser)
          .then ( res => {
              stats = res
          })
@@ -186,9 +197,9 @@ const WhoSeesWhat = () => {
          return stats 
     }
 
-    const fetchAdsPart2 =  async (age, gender, social_issue) => {
+    const fetchAdsPart2 =  async (age, gender, social_issue, advertiser) => {
         let stats 
-        await api.get(`api/demographicDistribution/entitiesTargetingAgeGender/`+age+`/`+gender+`/`+social_issue)
+        await api.get(`api/demographicDistribution/entitiesTargetingAgeGender/`+age+`/`+gender+`/`+social_issue+`/`+advertiser)
          .then ( res => {
              stats = res
          })
@@ -197,9 +208,9 @@ const WhoSeesWhat = () => {
          )
          return stats 
     }
-    const fetchAdsPerSocialIssuesPart1 =  async (age,gender) => {
+    const fetchAdsPerSocialIssuesPart1 =  async (age,gender,advertiser) => {
         let stats 
-        await api.get(`api/demographicDistribution/entitiesTargetingAgeGenderBySocialIssues/`+age+`/`+gender)
+        await api.get(`api/demographicDistribution/entitiesTargetingAgeGenderBySocialIssues/`+age+`/`+gender+`/`+advertiser)
          .then ( res => {
              stats = res
          })
@@ -209,9 +220,9 @@ const WhoSeesWhat = () => {
          return stats 
     }
 
-    const fetchAdsPerSocialIssuesPart2 =  async (age,gender) => {
+    const fetchAdsPerSocialIssuesPart2 =  async (age,gender,advertiser) => {
         let stats 
-        await api.get(`api/demographicDistribution/entitiesTargetingAgeGenderBySocialIssues/`+age+`/`+gender)
+        await api.get(`api/demographicDistribution/entitiesTargetingAgeGenderBySocialIssues/`+age+`/`+gender+`/`+advertiser)
          .then ( res => {
              stats = res
          })
@@ -262,12 +273,13 @@ const WhoSeesWhat = () => {
                   
                 <br/>      <br/>
                 <Col xl="5" sm="12" >
-                    <div style={{display:"flex", justifyContent:"space-around"}}> 
-                        <select value={adsPerSocialIssuesPart1.gender} onChange={(event) => loadAdsPerSocialIssuesPart1(adsPerSocialIssuesPart1.age,event.target.value)}>
+                   
+                        <div style={{display:"flex", justifyContent:"space-around"}}> 
+                        <select value={adsPerSocialIssuesPart1.gender} onChange={(event) => loadAdsPerSocialIssuesPart1(adsPerSocialIssuesPart1.age,event.target.value,adsPerSocialIssuesPart1.advertiser)}>
                             <option value="female">{intl.formatMessage({ id: 'female' })} </option>
                             <option value="male">{intl.formatMessage({ id: 'male' })} </option>
                         </select>
-                        <select  value={adsPerSocialIssuesPart1.age} onChange={(event) => loadAdsPerSocialIssuesPart1(event.target.value,adsPerSocialIssuesPart1.gender)}>
+                        <select  value={adsPerSocialIssuesPart1.age} onChange={(event) => loadAdsPerSocialIssuesPart1(event.target.value,adsPerSocialIssuesPart1.gender,adsPerSocialIssuesPart1.advertiser)}>
                             <option value="13-17">13-17</option>
                             <option value="18-24">18-24</option>
                             <option value="25-34">25-34</option>
@@ -276,7 +288,18 @@ const WhoSeesWhat = () => {
                             <option value="55-64">55-64</option>
                             <option value="65+">65+</option>
                         </select>
-                    </div> 
+                        </div> 
+                       
+                        <div style={{display:"flex", justifyContent:"center"}}> 
+
+                       <select  value={adsPerSocialIssuesPart1.advertiser} onChange={(event) => loadAdsPerSocialIssuesPart1(adsPerSocialIssuesPart1.age,adsPerSocialIssuesPart1.gender,event.target.value)}>
+                               {
+                                TopAdvertisers.map( (ad) => 
+                                <option value={ad.page_id}>{ad.page_name}</option> )
+                            }
+                          
+                        </select>
+                    </div>
                     <br/>
                     <div style={{height:"80vh", width:"100%"}}>
                      {
@@ -316,11 +339,11 @@ const WhoSeesWhat = () => {
                 </Col>
                 <Col xl="6" sm="12">
                     <div style={{display:"flex",justifyContent:"space-around"}}> 
-                        <select value={adsPerSocialIssuesPart2.gender} onChange={(event) => loadAdsPerSocialIssuesPart2(adsPerSocialIssuesPart2.age,event.target.value)}>
+                        <select value={adsPerSocialIssuesPart2.gender} onChange={(event) => loadAdsPerSocialIssuesPart2(adsPerSocialIssuesPart2.age,event.target.value,adsPerSocialIssuesPart2.advertiser)}>
                             <option value="female">{intl.formatMessage({ id: 'female' })} </option>
                             <option value="male">{intl.formatMessage({ id: 'male' })} </option>
                         </select>
-                        <select  value={adsPerSocialIssuesPart2.age} onChange={(event) => loadAdsPerSocialIssuesPart2(event.target.value,adsPerSocialIssuesPart2.gender)}>
+                        <select  value={adsPerSocialIssuesPart2.age} onChange={(event) => loadAdsPerSocialIssuesPart2(event.target.value,adsPerSocialIssuesPart2.gender,adsPerSocialIssuesPart2.advertiser)}>
                             <option value="13-17">13-17</option>
                             <option value="18-24">18-24</option>
                             <option value="25-34">25-34</option>
@@ -330,6 +353,16 @@ const WhoSeesWhat = () => {
                             <option value="65+">65+</option>
                         </select>
                     </div> 
+                    <div style={{display:"flex", justifyContent:"center"}}> 
+
+                            <select  value={adsPerSocialIssuesPart2.advertiser} onChange={(event) => loadAdsPerSocialIssuesPart2(adsPerSocialIssuesPart2.age,adsPerSocialIssuesPart2.gender,event.target.value)}>
+                                    {
+                                    TopAdvertisers.map( (ad) => 
+                                    <option value={ad.page_id}>{ad.page_name}</option> )
+                                }
+                            
+                            </select>
+                            </div>
                     <br/>
                     <div style={{height:"80vh", width:"100%"}}>
                      {
